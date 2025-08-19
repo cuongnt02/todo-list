b2c1a19c2d863242cdbe43611ba604af2b66a323
cuongnt02 <cuong.ntc02@gmail.com>
2025-08-19 23:18:16 +0700

    [1908] Specced first feature - add unit test to it

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,40 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
 
-assert "Congratulations!" in browser.title
-print("OK!")
+class NewVisitorTest(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_start_a_todo_list(self):
+        # Edith has heard about a new online to-do app
+        # She goes to check out its homepage
+        self.browser.get("http://localhost:8000")
+
+        # She notices the page title and header mentions to-do lists
+        self.assertIn("To-Do", self.browser.title)
+
+
+        # She is invited to enter a to-do item straight away
+        self.fail("Finish the tests!")
+
+
+        # She types "buy peacock feather" into a text box
+        # Edith's hobby is tying fly-fishing lures
+
+        # When she hits enter, the pages updates, and now the page lists
+        # "1. Buy peacock feathers" as an item in to-do list
+
+        # There is still a text box inviting her to add another item
+        # She enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+        # The page updates again, and now show both items on her list 
+
+        # Satisfied, she goes back to sleep
+
+if __name__ == "__main__":
+    unittest.main()
