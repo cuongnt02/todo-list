830592249d62e52b00a0ca9a7e8187b295d35b3d
cuongnt02 <cuong.ntc02@gmail.com>
2025-08-22 16:04:22 +0700

    [2208] Forms in home page now can handle POST request

--- functional_tests.py
+++ functional_tests.py
@@ -30,9 +30,9 @@ class NewVisitorTest(unittest.TestCase):
         self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
 
 
-        # She types "Buy peacock feather" into a text box
+        # She types "Buy peacock feathers" into a text box
         # Edith's hobby is tying fly-fishing lures
-        input_box.send_keys('Buy peacock feather')
+        input_box.send_keys('Buy peacock feathers')
 
         # When she hits enter, the pages updates, and now the page lists
         # "1. Buy peacock feathers" as an item in to-do list
@@ -41,12 +41,24 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feather' for row in rows), msg="New to-do item did not appear in table.")
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # There is still a text box inviting her to add another item
+        input_box = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
+
         # She enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        input_box.send_keys('Use peacock feathers to make a fly')
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+
         # The page updates again, and now show both items on her list 
-        self.fail('Finish the tests!')
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+
         # Satisfied, she goes back to sleep
 
 if __name__ == "__main__":
--- lists/tests.py
+++ lists/tests.py
@@ -10,3 +10,13 @@ class HomePageTest(TestCase):
     def test_renders_homepage_content(self):
         response = self.client.get('/')
         self.assertContains(response, 'To-Do')
+
+    def test_renders_input_form(self):
+        response = self.client.get('/')
+        self.assertContains(response, '<form method="POST">')
+        self.assertContains(response, 'name="item_text"')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={"item_text":"A new list item"})
+        self.assertContains(response, "A new list item")
+        self.assertTemplateUsed(response, 'home.html')
