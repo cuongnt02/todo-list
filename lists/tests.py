8bf2924ff3bc33332d9d5d138c06b02d36e77c02
cuongnt02 <cuong.ntc02@gmail.com>
2025-08-23 16:39:48 +0700

    [2308] Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,8 @@
 from django.http import HttpRequest
 from django.test import TestCase
 from lists.views import home_page
+from lists.models import Item
+
 
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
@@ -16,7 +18,44 @@ class HomePageTest(TestCase):
         self.assertContains(response, '<form method="POST">')
         self.assertContains(response, 'name="item_text"')
 
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemmey 1')
+        Item.objects.create(text='itemmey 2')
+
+        response = self.client.get('/')
+
+        self.assertContains(response, 'itemmey 1')
+        self.assertContains(response, 'itemmey 2')
+
     def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data={"item_text":"A new list item"})
-        self.assertContains(response, "A new list item")
-        self.assertTemplateUsed(response, 'home.html')
+        self.client.post('/', data={"item_text": "A new list item"})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={"item_text": "A new list item"})
+        self.assertRedirects(response, '/')
+
+    def test_only_save_item_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_item(self):
+        first_item = Item()
+        first_item.text = "The first (ever) list item"
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "Item the second"
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
