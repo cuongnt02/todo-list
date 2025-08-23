8def4dc531b4b3005d5aa98a94e198188bb3ae2a
cuongnt02 <cuong.ntc02@gmail.com>
2025-08-24 00:06:46 +0700

    [2408] Use LiveServerTestCase to make functional_tests an app

---
+++ functional_tests/tests.py
@@ -0,0 +1,61 @@
+from selenium import webdriver
+from django.test import LiveServerTestCase
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+class NewVisitorTest(LiveServerTestCase):
+
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
+    def test_can_start_a_todo_list(self):
+        # Edith has heard about a new online to-do app
+        # She goes to check out its homepage
+        self.browser.get(self.live_server_url)
+
+        # She notices the page title and header mentions to-do lists
+        self.assertIn("To-Do", self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, "h1").text
+        self.assertIn("To-Do", header_text)
+
+        # She is invited to enter a to-do item straight away
+        input_box = self.browser.find_element(By.ID, "id_new_item")
+        self.assertEqual(input_box.get_attribute("placeholder"), "Enter a to-do item")
+
+        # She types "Buy peacock feathers" into a text box
+        # Edith's hobby is tying fly-fishing lures
+        input_box.send_keys("Buy peacock feathers")
+
+        # When she hits enter, the pages updates, and now the page lists
+        # "1. Buy peacock feathers" as an item in to-do list
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+
+        # There is still a text box inviting her to add another item
+        input_box = self.browser.find_element(By.ID, "id_new_item")
+        self.assertEqual(input_box.get_attribute("placeholder"), "Enter a to-do item")
+
+        # She enters "Use peacock feathers to make a fly"
+        # (Edith is very methodical)
+        input_box.send_keys("Use peacock feathers to make a fly")
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # The page updates again, and now show both items on her list
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+
+        # Satisfied, she goes back to sleep
