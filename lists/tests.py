b996c1f1c6ed1825b150317b903466b4adef32ce
cuongnt02 <cuong.ntc02@gmail.com>
2025-08-25 12:08:15 +0700

    [2508] New route to create list, map requests and forms to new route

--- lists/tests.py
+++ lists/tests.py
@@ -11,22 +11,8 @@ class HomePageTest(TestCase):
 
     def test_renders_input_form(self):
         response = self.client.get("/")
-        self.assertContains(response, '<form method="POST" action="/">')
+        self.assertContains(response, '<form method="POST" action="/lists/new">')
         self.assertContains(response, 'name="item_text"')
-
-    def test_can_save_a_POST_request(self):
-        self.client.post("/", data={"item_text": "A new list item"})
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, "A new list item")
-
-    def test_redirects_after_POST(self):
-        response = self.client.post("/", data={"item_text": "A new list item"})
-        self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
-
-    def test_only_save_item_when_necessary(self):
-        self.client.get("/")
-        self.assertEqual(Item.objects.count(), 0)
 
 
 class ListViewTest(TestCase):
@@ -36,7 +22,7 @@ class ListViewTest(TestCase):
 
     def test_renders_input_form(self):
         response = self.client.get("/lists/the-only-list-in-the-world/")
-        self.assertContains(response, '<form method="POST" action="/">')
+        self.assertContains(response, '<form method="POST" action="/lists/new">')
         self.assertContains(response, 'name="item_text"')
 
     def test_displays_all_list_items(self):
@@ -47,6 +33,18 @@ class ListViewTest(TestCase):
 
         self.assertContains(response, "itemmey 1")
         self.assertContains(response, "itemmey 2")
+
+
+class NewListTest(TestCase):
+    def test_can_save_a_POST_request(self):
+        self.client.post("/lists/new", data={"item_text": "A new list item"})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
+
+    def test_redirects_after_POST(self):
+        response = self.client.post("/lists/new", data={"item_text": "A new list item"})
+        self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
 
 
 class ItemModelTest(TestCase):
