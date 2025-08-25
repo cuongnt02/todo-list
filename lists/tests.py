2ad4223e8f4c108cf39dd07464f3873de023b96e
cuongnt02 <cuong.ntc02@gmail.com>
2025-08-25 14:16:25 +0700

    [2508] Add new route adding new item to existing list, passes FT

--- lists/tests.py
+++ lists/tests.py
@@ -17,23 +17,30 @@ class HomePageTest(TestCase):
 
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
-        response = self.client.get("/lists/the-only-list-in-the-world/")
+        my_list = List.objects.create()
+        response = self.client.get(f"/lists/{my_list.id}/")
         self.assertTemplateUsed(response, "list.html")
 
     def test_renders_input_form(self):
-        response = self.client.get("/lists/the-only-list-in-the-world/")
-        self.assertContains(response, '<form method="POST" action="/lists/new">')
+        my_list = List.objects.create()
+        response = self.client.get(f"/lists/{my_list.id}/")
+        self.assertContains(
+            response, f'<form method="POST" action="/lists/{my_list.id}/add_item">'
+        )
         self.assertContains(response, 'name="item_text"')
 
-    def test_displays_all_list_items(self):
-        my_list = List.objects.create()
-        Item.objects.create(text="itemmey 1", list=my_list)
-        Item.objects.create(text="itemmey 2", list=my_list)
+    def test_displays_only_items_for_that_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text="itemmey 1", list=correct_list)
+        Item.objects.create(text="itemmey 2", list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text="other list item", list=other_list)
 
-        response = self.client.get("/lists/the-only-list-in-the-world/")
+        response = self.client.get(f"/lists/{correct_list.id}/")
 
         self.assertContains(response, "itemmey 1")
         self.assertContains(response, "itemmey 2")
+        self.assertNotContains(response, "other list item")
 
 
 class NewListTest(TestCase):
@@ -45,7 +52,34 @@ class NewListTest(TestCase):
 
     def test_redirects_after_POST(self):
         response = self.client.post("/lists/new", data={"item_text": "A new list item"})
-        self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
+        my_list = List.objects.get()
+        self.assertRedirects(response, f"/lists/{my_list.id}/")
+
+
+class NewItemTest(TestCase):
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            f"/lists/{correct_list.id}/add_item",
+            data={"item_text": "A new item for the existing list"},
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.get()
+        self.assertEqual(new_item.text, "A new item for the existing list")
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            f"/lists/{correct_list.id}/add_item", data={"item_text": "A new list item"}
+        )
+
+        self.assertRedirects(response, f"/lists/{correct_list.id}/")
 
 
 class ItemModelTest(TestCase):
--- lists/views.py
+++ lists/views.py
@@ -7,12 +7,19 @@ def home_page(request: HttpRequest):
     return render(request, "home.html")
 
 
-def view_list(request: HttpRequest):
-    items = Item.objects.all()
-    return render(request, "list.html", {"items": items})
+def view_list(request: HttpRequest, list_id):
+    our_list = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=our_list)
+    return render(request, "list.html", {"list": our_list})
+
+
+def add_item(request: HttpRequest, list_id):
+    our_list = List.objects.get(id=list_id)
+    item = Item.objects.create(text=request.POST["item_text"], list=our_list)
+    return redirect(f"/lists/{our_list.id}/")
 
 
 def new_list(request: HttpRequest):
     nulist = List.objects.create()
     Item.objects.create(text=request.POST["item_text"], list=nulist)
-    return redirect("/lists/the-only-list-in-the-world/")
+    return redirect(f"/lists/{nulist.id}/")
