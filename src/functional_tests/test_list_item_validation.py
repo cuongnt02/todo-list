e00f20d3328b71981a604f07e504bd7bd6aa92f5
cuongnt02 <cuong.ntc02@gmail.com>
2025-08-30 13:55:23 +0700

    [3008] Move unit tests into a folder with single file

--- src/functional_tests/test_list_item_validation.py
+++ src/functional_tests/test_list_item_validation.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from functional_tests.base import FunctionalTest
 
 
@@ -6,11 +8,20 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         # Edith goes to the home page and accidentally tries to submit
         # an empty list item. She hits Enter on the empty input box
+        self.browser.get(self.live_server_url)
+        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
 
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
+        self.wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_element(By.CSS_SELECTOR, ".invalid-feedback").text,
+                "You can't have an empty list item",
+            )
+        )
 
         # She tries again with some text for the item, which now works
+        self.fail("finish the test")
 
         # Perversely, she now decides to submit a second blank list item
 
