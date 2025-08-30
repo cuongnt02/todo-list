13476004dba9e029b6dc48e389b8b5690c04a315
cuongnt02 <cuong.ntc02@gmail.com>
2025-08-30 14:00:32 +0700

    [3008] Split out unit test into two separate files

--- src/lists/tests/test_views.py
+++ src/lists/tests/test_views.py
@@ -1,6 +1,4 @@
-from django.http import HttpRequest
 from django.test import TestCase
-from lists.views import home_page
 from lists.models import Item, List
 import lxml.html
 
@@ -84,32 +82,3 @@ class NewItemTest(TestCase):
         )
 
         self.assertRedirects(response, f"/lists/{correct_list.id}/")
-
-
-class ItemModelTest(TestCase):
-    def test_saving_and_retrieving_item(self):
-        my_list = List()
-        my_list.save()
-
-        first_item = Item()
-        first_item.text = "The first (ever) list item"
-        first_item.list = my_list
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = "Item the second"
-        second_item.list = my_list
-        second_item.save()
-
-        saved_list = List.objects.get()
-        self.assertEqual(my_list, my_list)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, "The first (ever) list item")
-        self.assertEqual(first_saved_item.list, my_list)
-        self.assertEqual(second_saved_item.text, "Item the second")
-        self.assertEqual(second_saved_item.list, my_list)
